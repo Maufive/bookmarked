f1dda31c45014c4fcf23b50ff700d84a870233fa
Niklas Albinsson <albinssonniklas@gmail.com>
2023-05-28 00:13:32 +0200

    Add google auth provider

--- src/components/user-auth-form.tsx
+++ src/components/user-auth-form.tsx
@@ -9,14 +9,33 @@ import { userAuthSchema } from '@/lib/validations/auth';
 import { buttonVariants } from '@/components/ui/button';
 import Spinner from '@/components/ui/spinner';
 import GithubIcon from './ui/github-icon';
+import GoogleIcon from './ui/google-icon';
+import { Loader2 } from 'lucide-react';
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 export function UserAuthForm({ className }: UserAuthFormProps) {
   const [isGitHubLoading, setIsGitHubLoading] = React.useState<boolean>(false);
+  const [isGoogleLoading, setIsGoogleLoading] = React.useState<boolean>(false);
 
   return (
-    <div className={cn('grid gap-6', className)}>
+    <div className={cn('grid gap-4', className)}>
+      <button
+        type="button"
+        className={cn(buttonVariants({ variant: 'default' }))}
+        onClick={() => {
+          setIsGoogleLoading(true);
+          signIn('google');
+        }}
+        disabled={isGoogleLoading}
+      >
+        {isGoogleLoading ? (
+          <Loader2 className="mr-2 h-4 w-4 animate-spin text-primary-on-brand" />
+        ) : (
+          <GoogleIcon className="mr-2 h-4 w-4" />
+        )}{' '}
+        Continue with Google
+      </button>
       <button
         type="button"
         className={cn(buttonVariants({ variant: 'outline' }))}
@@ -27,11 +46,11 @@ export function UserAuthForm({ className }: UserAuthFormProps) {
         disabled={isGitHubLoading}
       >
         {isGitHubLoading ? (
-          <Spinner />
+          <Loader2 className="mr-2 h-4 w-4 animate-spin text-primary-on-brand" />
         ) : (
           <GithubIcon className="mr-2 h-4 w-4" />
-        )}{' '}
-        Github
+        )}
+        Continue with Github
       </button>
     </div>
   );
