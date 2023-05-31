1a1c49676233cacda4e7e2a6aac330febe727e9e
Niklas Albinsson <albinssonniklas@gmail.com>
2023-05-31 20:33:59 +0200

    CSS changes

--- src/components/page-layout.tsx
+++ src/components/page-layout.tsx
@@ -5,7 +5,7 @@ type PageLayoutProps = {
 export default function PageLayout({ children }: PageLayoutProps) {
   return (
     <main className="background-gradient min-h-screen p-4 md:p-12">
-      <section className="mx-auto mt-[56px] flex flex-col gap-4 space-y-6 pb-8 pt-6 md:pb-12 lg:max-w-3xl">
+      <section className="mx-auto mt-[56px] flex flex-col gap-4 space-y-6 pb-[72px] pt-6 md:pb-12 lg:max-w-3xl">
         {children}
       </section>
     </main>
