ac2ff7ed35cb42686913fbb0fd7c08f46a0eb52a
Niklas Albinsson <albinssonniklas@gmail.com>
2024-02-22 20:04:54 +0100

    Add log for cron job

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -2,6 +2,7 @@ import NextResponse from "next/server";
 import { db } from "@/lib/db";
 
 export async function GET() {
-  const users = db.user.findMany();
+  const users = await db.user.findMany();
+  console.log("Queried users", users.length);
   return NextResponse.NextResponse.json({ users });
 }
