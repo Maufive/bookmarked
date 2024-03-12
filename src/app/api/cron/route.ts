5080dc05fea2e09470fe16a5b70cce0453b37853
Niklas Albinsson <albinssonniklas@gmail.com>
2024-03-12 18:41:38 +0100

    Remove CRON job

--- src/app/api/cron/route.ts
+++
@@ -1,8 +0,0 @@
-import NextResponse from "next/server";
-import { db } from "@/lib/db";
-
-export async function GET() {
-  const users = await db.user.findMany();
-  console.log("Queried users", users.length);
-  return NextResponse.NextResponse.json({ users });
-}
